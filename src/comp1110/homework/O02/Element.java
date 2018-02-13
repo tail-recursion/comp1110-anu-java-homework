011e96f6cc1eacfa0e2a1a4bdb4f1786aabeeaab
Dale <>
2018-02-13 12:48:30 +1100

    Implement O02

--- src/comp1110/homework/O02/Element.java
+++ src/comp1110/homework/O02/Element.java
@@ -1,4 +1,36 @@
 package comp1110.homework.O02;
 
-public class Element {
+public enum Element {
+    /*
+    create an enum `Element` that stores information about chemical elements.  You
+    only need to implement the first five elements, and you only need to record
+    the atomic weight, the group, and the period (listed in the table below).
+    * The enum will have a constructor with signature `Element(int weight, int group, int period)`.
+    * The enum will have a private instance field `facts` that is an array of `int`
+    * The constructor will initialize the instance field to be an array of size three,
+    and then initialize its elements to be the weight, group, and period of
+    the element, respectively.
+    * The enum will have a getter method `getFacts()` that returns the `int[]`, representing
+    the three values: weight, group, and period for the given elment.
+    * Implement this class (if you wish you may implement a main method that demonstrates its use).
+
+    Once you have it working, add and commit your work.
+
+    | Element   | Weight   |  Group  | Period  |
+    | --------- | -------- | ------- | ------- |
+    | H         | 1        | 1       | 1       |
+    | He        | 2        | 18      | 1       |
+    | Li        | 3        | 1       | 2       |
+    | Be        | 4        | 2       | 2       |
+    | B         | 5        | 13      | 2       |
+     */
+    H(1,1,1), He(2,18,1), Li(3,1,2), Be(4,2,2), B(5,13,2);
+    private int[] facts;//weight,group,period;
+    Element(int weight, int group, int period) {
+        facts=new int[3];
+        facts[0]=weight;
+        facts[1]=group;
+        facts[2]=period;
+    }
+    int[] getFacts() { return facts; }
 }
